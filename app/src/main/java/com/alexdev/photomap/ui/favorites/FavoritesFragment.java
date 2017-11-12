b89be8f5f49ebef9d872c5513ac4961a14c95a5b
alex <sashaudalv@gmail.com>
2017-11-12 15:37:26 +0300

    added staggered grid to favorites list at landscape orientation

--- app/src/main/java/com/alexdev/photomap/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/com/alexdev/photomap/ui/favorites/FavoritesFragment.java
@@ -2,6 +2,7 @@ package com.alexdev.photomap.ui.favorites;
 
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.util.Pair;
@@ -10,6 +11,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.Adapter;
 import android.support.v7.widget.RecyclerView.LayoutManager;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,14 +40,17 @@ public class FavoritesFragment extends Fragment implements ReselectableFragment,
     SwipeRefreshLayout mSwipeRefreshLayout;
     private LayoutManager mLayoutManager;
     private Adapter mAdapter;
-    private List<Pair<User, Photo>> mFavoritesList;
+    private List<Pair<User, Photo>> mFavoritesList = new ArrayList<>();
 
     {
-        mFavoritesList = new ArrayList<>();
         mFavoritesList.add(new Pair<>(new User(1, "pavel", "durov", "https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png"),
-                new Photo(1, "https://b1.filmpro.ru/c/455897.700xp.jpg", 1, "Sample text", 1509980225786L, 0L, true)));
+                new Photo(1, "https://b1.filmpro.ru/c/455897.700xp.jpg", 1, "Lorem ipsum dolor sit amet", 1509980225786L, 0L, true)));
         mFavoritesList.add(new Pair<>(new User(150150, "nik", "safronov", "https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png"),
-                new Photo(2, "https://b1.filmpro.ru/c/455897.700xp.jpg", 150150, null, 1509980225786L, 0L, true)));
+                new Photo(2, "https://b1.filmpro.ru/c/455897.700xp.jpg", 150150, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit", 1509980225786L, 0L, true)));
+        mFavoritesList.add(new Pair<>(new User(1, "pavel", "durov", "https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png"),
+                new Photo(1, "https://b1.filmpro.ru/c/45587.700xp.jpg", 1, "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", 1509980225786L, 0L, true)));
+        mFavoritesList.add(new Pair<>(new User(150150, "nik", "safronov", "https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png"),
+                new Photo(2, "https://b1.filmpro.ru/c/45897.700xp.jpg", 150150, null, 1509980225786L, 0L, true)));
     }
 
     public FavoritesFragment() {
@@ -66,7 +71,14 @@ public class FavoritesFragment extends Fragment implements ReselectableFragment,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favorites, container, false);
         ButterKnife.bind(this, view);
-        mLayoutManager = new LinearLayoutManager(getContext());
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mLayoutManager = new LinearLayoutManager(getContext());
+        } else {
+            mLayoutManager = new StaggeredGridLayoutManager(
+                    getResources().getInteger(R.integer.landscape_grid_span_count),
+                    StaggeredGridLayoutManager.VERTICAL
+            );
+        }
         mFavoritesRV.setLayoutManager(mLayoutManager);
         mAdapter = new FavoritesListRVAdapter(mFavoritesList, getContext(), this);
         mFavoritesRV.setAdapter(mAdapter);
@@ -81,7 +93,7 @@ public class FavoritesFragment extends Fragment implements ReselectableFragment,
     }
 
     private void onRefresh() {
-        //TODO do smth and update state
+        //TODO try to upload data from db
     }
 
     @Override
